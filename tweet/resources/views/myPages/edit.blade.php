c7698faac491c9eff27ccc85dc8406d5acc1a127
ishii kunitaka <ishii@inter-edu.com>
2019-03-11 21:30:16 +0900

    値が空だった時の処理を追加。 生年月日のプルダウンメニューを追加しました。

--- tweet/resources/views/myPages/edit.blade.php
+++ tweet/resources/views/myPages/edit.blade.php
@@ -13,31 +13,40 @@
             <table>
                 <tr>
                     <td>{{ Form::label('name','名前') }}</td>
-                    <td>{{ Form::text('name', old('name', $userInfo->name)) }}</td>
+                    <td>{{ Form::text('name', old('name', isset($userInfo->name)? $userInfo->name : '')) }}</td>
                 </tr>
                 <tr>
                     <td>{{ Form::label('email','メールアドレス') }}</td>
-                    <td>{{ Form::text('email', old('email', $userInfo->email)) }}</td>
+                    <td>{{ Form::text('email', old('email', isset($userInfo->email)? $userInfo->email : '')) }}</td>
                 </tr>
                 <tr>
                     <td>{{ Form::label('profile', '自己紹介') }}</td>
-                    <td>{{ Form::textarea('profile', old('profile', $userInfo->profile, ['size' => '30x3'])) }}</td>
+                    <td>{{ Form::textarea('profile', old('profile', isset($userInfo->profile)? $userInfo->profile : '', ['size' => '30x3'])) }}</td>
+                </tr>
+                <tr><td>生年月日</td></tr>
+                <tr>
+                    <td>{{ Form::label('year', '年') }}</td>
+                    <td>{{ Form::selectRange('year', 1900, 2019) }}年</td>
                 </tr>
                 <tr>
-                <!-- 生年月日、月、日を別々のプルダウンメニューで選択式にする。 -->
-               
+                    <td>{{ Form::label('month', '月') }}</td>
+                    <td>{{ Form::selectRange('month', 1, 12) }}月</td>
+                </tr>
+                <tr>
+                    <td>{{ Form::label('day', '日') }}</td>
+                    <td>{{ Form::selectRange('day', 1, 31) }}日</td>
                 </tr>
                 <tr>
                     <td>{{ Form::label('residence', '居住地') }}</td>
-                    <td>{{ Form::text('residence', old('residence', $userInfo->residence)) }}</td>
+                    <td>{{ Form::text('residence', old('residence', isset($userInfo->residence)? $userInfo->residence : '')) }}</td>
                 </tr>
                 <tr>
                     <td>{{ Form::label('blood_type', '血液型') }}</td>
-                    <td>{{ Form::text('blood_type', old('blood_type', $userInfo->blood_type)) }}</td>
+                    <td>{{ Form::text('blood_type', old('blood_type', isset($userInfo->blood_type)? $userInfo->blood_type : '')) }}</td>
                 </tr>
                 <tr>
                     <td>{{ Form::label('hobby', '趣味') }}</td>
-                    <td>{{ Form::text('hobby', old('hobby', $userInfo->hobby)) }}</td>
+                    <td>{{ Form::text('hobby', old('hobby', isset($userInfo->hobby)? $userInfo->hobby : '')) }}</td>
                 </tr>
             </table>
             {{ Form::hidden('user_id', "$user->id") }}
--- tweet/resources/views/myPages/userInfo.blade.php
+++ tweet/resources/views/myPages/userInfo.blade.php
@@ -11,11 +11,11 @@
     <table>
         <tr><td><span>名前：</span></td><td>{{ $user->name }}</td></tr>
         <tr><td><span>登録メールアドレス：</span></td><td>{{ $user->email }}</td></tr>
-        <tr><th>自己紹介</th><td>{{ $userInfo->profile }}</td></tr>
+        <tr><th>自己紹介</th><td>{{ isset($userInfo->profile)? $userInfo->profile : '' }}</td></tr>
         <tr><th>生年月日</th><td>本文が入るスペース</td></tr>
-        <tr><th>居住地</th><td>{{ $userInfo->residence }}</td></tr>
-        <tr><th>血液型</th><td>{{ $userInfo->blood_type }}</td></tr>
-        <tr><th>趣味</th><td>{{ $userInfo->hobby }}</td></tr>
+        <tr><th>居住地</th><td>{{ isset($userInfo->residence)? $userInfo->residence : '' }}</td></tr>
+        <tr><th>血液型</th><td>{{ isset($userInfo->blood_type)? $userInfo->blood_type : '' }}</td></tr>
+        <tr><th>趣味</th><td>{{ isset($userInfo->hobby)? $userInfo->hobby : '' }}</td></tr>
     </table>
     <a href="/mypage/edit">ユーザー情報を変更する</a><br>
     <a href="/mypage/passwordEdit">パスワードを変更する</a><br>
