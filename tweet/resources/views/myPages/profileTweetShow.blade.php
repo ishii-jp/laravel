ad92a0d002ccf3774a6fd44221f1c5d616039437
ishii kunitaka <ishii@inter-edu.com>
2019-04-22 20:24:45 +0900

    コミットし忘れ

--- tweet/resources/views/myPages/profileTweetShow.blade.php
+++ tweet/resources/views/myPages/profileTweetShow.blade.php
@@ -11,27 +11,29 @@
 @section('title', $title)
 
 @section('content')
-    <p>{{ $user->name }}さんの投稿一覧</p>
-    <table>
-        <tr>
-            <th>投稿者　</th><th>タイトル　</th><th>本文　</th><th>投稿日時　</th>
-        </tr>
-        @foreach ($user->tweets as $tweet)
+    <div class="container">
+        <p>{{ $user->name }}さんの投稿一覧</p>
+        <table class="table table-striped">
             <tr>
-                <td>{{ $tweet->user->name }}</td>
-                <td>{{ isset($tweet->title)? $tweet->title : '' }}</td>
-                <td>{{ $tweet->text }}</td>
-                <td>
-                    @foreach ($tweet->tweetImages as $tweetImage)
-                        @isset($tweetImage->image)
-                            <img id="profile_img" src="{{ asset('storage/avatar/'. $tweetImage->image) }}" alt="avatar" />
-                        @endisset
-                    @endforeach
-                </td>
-                <td>{{ $tweet->updated_at }}</td>
+                <th>投稿者</th><th>タイトル</th><th>本文</th><th>投稿日時</th><th></th>
             </tr>
-        @endforeach
-    </table>
+            @foreach ($user->tweets as $tweet)
+                <tr>
+                    <td>{{ $tweet->user->name }}</td>
+                    <td>{{ isset($tweet->title)? $tweet->title : '' }}</td>
+                    <td>{{ $tweet->text }}</td>
+                    <td>
+                        @foreach ($tweet->tweetImages as $tweetImage)
+                            @isset($tweetImage->image)
+                                <img id="profile_img" src="{{ asset('storage/avatar/'. $tweetImage->image) }}" alt="avatar" />
+                            @endisset
+                        @endforeach
+                    </td>
+                    <td>{{ $tweet->updated_at }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
 
 @section('footer')
