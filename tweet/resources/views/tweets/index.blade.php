1fb9adc67405656dd716bc580d5311566ce7a7f0
ishii kunitaka <ishii@inter-edu.com>
2019-08-08 17:26:23 +0900

    endifの位置を修正しました。

--- tweet/resources/views/tweets/index.blade.php
+++ tweet/resources/views/tweets/index.blade.php
@@ -34,10 +34,10 @@
             <p>検索結果はありませんでした。</p>
         @else
             <table class="table table-striped">
-                    <tr>
-                        <th>投稿者</th><th>タイトル</th><th>本文</th><th>投稿日時</th><th></th><th></th><th></th><th></th>
-                    </tr>
-                    @foreach ($tweets as $tweet)
+                <tr>
+                    <th>投稿者</th><th>タイトル</th><th>本文</th><th>投稿日時</th><th></th><th></th><th></th><th></th>
+                </tr>
+                @foreach ($tweets as $tweet)
                     <tr>
                         <td><a href="/mypage/profile/{{ $tweet->user->id }}">{{ $tweet->user->name }}</a></td>
                         <td>{{ isset($tweet->title)? nl2br(e($tweet->title)) : '' }}</td>
@@ -49,12 +49,12 @@
                                 @endisset
                             @endforeach
                         </td>
-                        
+
                         <td>{{ $tweet->updated_at }}</td>
                         <td><a href="/reply/{{ $tweet->id }}" class="btn btn-default active" role="button">返信</a></td>
                         <td><a href="/reply/show/{{ $tweet->id }}" class="btn btn-default active" role="button">この投稿への返信</a></td>
-        {{--                ここにこのツイートに対する返信があった場合はリンクを出力して、リンク先の画面で親ツイートと返信ツイートを全て出力します。--}}
-        {{--                @php var_dump($tweet->user); @endphp--}}
+                        {{--                ここにこのツイートに対する返信があった場合はリンクを出力して、リンク先の画面で親ツイートと返信ツイートを全て出力します。--}}
+                        {{--                @php var_dump($tweet->user); @endphp--}}
                         @if ($tweet->likes->isEmpty())
                             <td>
                                 <button class="btn btn-success" id="like{{ $tweet->id }}" data-tweet_number="{{ $tweet->id }}">いいね！</button>
@@ -68,10 +68,10 @@
                             <a href="/like/userShow/{{ $tweet->id }}"><p class="text-success" id="likeCount{{ $tweet->id }}">いいね数{{ $tweet->likes_count }}</p></a>
                         </td>
                     </tr>
-                    @endforeach
+                @endforeach
             </table>
             {{ $tweets->links() }}
-            @include('likeRanking')
-        </div>
-    @endif
+        @endif
+        @include('likeRanking')
+    </div>
 @endsection
